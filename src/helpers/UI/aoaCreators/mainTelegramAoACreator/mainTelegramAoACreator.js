0557067fe01896cbcb85df83f0ae69cbd3d2465c
Alexey Mozyrskiy <mozyrski@gmail.com>
2021-04-06 17:44:35 +0300

    in function mainTelegramAoACreator third telegram row is ready

--- src/helpers/UI/aoaCreators/mainTelegramAoACreator/mainTelegramAoACreator.js
+++ src/helpers/UI/aoaCreators/mainTelegramAoACreator/mainTelegramAoACreator.js
@@ -25,7 +25,7 @@ export function createMainTelegramAoA(data) {
 
   let pchForSecondRow = data.uniquePch.map(pch => {                                       // массив со строками вида: ["ПЧ-1 Шахтинская дистанция пути", "ПЧ-1 Шахтинская дистанция пути"]
     const pchObj = DB.distances.find(distanceObj => distanceObj.distanceNumber === pch);  // найдем в базе данных объект с информацией об интересубщем нас ПЧ
-    const pchName = pchObj ? pchObj.distanceNameForTelegram : null;                        // Если нашли достанем из него название ПЧ для телеграммы
+    const pchName = pchObj ? pchObj.distanceFullName : null;                        // Если нашли достанем из него название ПЧ для телеграммы
     return pchName;
   }).join(", ");      // преобразуем массив в строку для корректного отображения в телеграмме
 
@@ -35,39 +35,64 @@ export function createMainTelegramAoA(data) {
 
 
   // ----------------------------------------- третья строка телеграммы -------------------------------------------------
-  const dataForThirdRow = data.uniquePch.map(distance => {            // данные для третьей строки телеграммы массив объектов
-    let distanceName;               // имя ПЧ например: "ПЧ-15"
-
-    if (distance !== 0) {                                                                             // если данные уже загружены
-      const distanceDataFromDB = DB.distances.find(item => item.distanceNumber === distance);         // найдем элемент с данными по ПЧ в Базе данных, чтобы вытянуть оттуда название ПЧ
-      const splitedDistanseName = distanceDataFromDB.distanceNameForTelegram.split(" ");              // сплитнем полное название ПЧ чтобы вытащить из него первый элемент, например: "ПЧ-15"
-      distanceName = splitedDistanseName[0];                                                    // имя ПЧ например: "ПЧ-15"
-    }
-
+  const dataForThirdRow = data.uniquePch.map(distance => {                    // данные для третьей строки телеграммы массив объектов
+    const distancePartAndNumber = getDistancePartAndNumber(distance, DB);       // имя ПЧ например: "ПЧ-15"
 
     const distanceDataFromSelector = data.generalStatistics.find(item => item.pch === distance);    // найдем элемент с данными с посчитанной статистикой по ПЧ
     const sumKm = (distanceDataFromSelector.otlKm + distanceDataFromSelector.xorKm + distanceDataFromSelector.UdKm + distanceDataFromSelector.neUdKm).toFixed(3);
 
-    return {distanceNumber: distance, distanceName, sumKm};             // вернем объект с данными для формирования массива объектов с данными по каждому ПЧ
+    return { distanceNumber: distance, distancePartAndNumber, sumKm };        // вернем объект с данными для формирования массива объектов с данными по каждому ПЧ
   });
 
-  const dataForThirdRowArr = dataForThirdRow.map(distance => `${distance.distanceName} - ${distance.sumKm} км`);                    //  данные для третьей строки телеграммы - простой массив, чтобы джйонуть его в строку
+  const dataForThirdRowArr = dataForThirdRow.map(distance => `${distance.distancePartAndNumber} - ${distance.sumKm} км`);                    //  данные для третьей строки телеграммы - простой массив, чтобы джйонуть его в строку
   const dataForThirdRowStr = dataForThirdRowArr.join("; ")
   const thirdRow = `2. ${data.typeOfCheck}: ${dataForThirdRowStr};`
   // ----------------------------------------- третья строка телеграммы -------------------------------------------------
 
 
   // ----------------------------------------- четвертая строка телеграммы -------------------------------------------------
-  
+  const fourthRow = "3. Сопровождение: согласно №ЦДИ-245/р от 25.03.2020 ЦДМ;";
   // ----------------------------------------- четвертая строка телеграммы -------------------------------------------------
 
 
+  // ----------------------------------------- пятая строка телеграммы -------------------------------------------------
+  const dataForFifthRow = data.uniquePch.map(distance => {
+    const distancePartAndNumber = getDistancePartAndNumber(distance, DB);       // имя ПЧ например: "ПЧ-15"
+
+    const currentIdstanceData = data.generalStatistics.find(item => item.pch === distance);   // найдем обехукт в расчитанных селектором данных с информацией по текущему ПЧ
+    let magnitudeN;
+    if(currentIdstanceData) {
+      magnitudeN = currentIdstanceData.magnitudeN;                                            // достанем из него Величину Nуч. по текущему ПЧ
+    }
+
+    return {distancePartAndNumber, magnitudeN}
+  });
+
+  const dataForFifthRowArr = dataForFifthRow.map(distance => `${distance.distancePartAndNumber} - ${distance.magnitudeN} ед.`);                    //  данные для третьей строки телеграммы - простой массив, чтобы джйонуть его в строку
+  const dataForFifthRowStr = dataForFifthRowArr.join("; ")
+  const fifthRow = `4. Величина Nуч: ${dataForFifthRowStr}`;
+  // ----------------------------------------- пятая строка телеграммы -------------------------------------------------
 
 
 
-  dataToWrite.push([secondRow], [firstRow], [thirdRow]);      // формируем телеграмму
+
+
+
+  dataToWrite.push([secondRow], [firstRow], [thirdRow], [fourthRow], [fifthRow]);      // формируем телеграмму
 
   debugger
 
   return dataToWrite;
+}
+
+
+
+
+function getDistancePartAndNumber(distanceNumber, DB) {
+  const distanceObj = DB.distances.find(item => item.distanceNumber === distanceNumber);        // объект из ДБ с информацие о ПЧ
+  if (distanceObj) {
+    return distanceObj.distancePartAndNumber;                                     // имя ПЧ например: "ПЧ-15"
+  } else {
+    return "";
+  }
 }
