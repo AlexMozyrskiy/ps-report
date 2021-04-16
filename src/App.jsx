f1a1e0a0baccf9e194a2ef68f0fe980608aa1ec6
Alexei Mozyrskiy <mozyrski@gmail.com>
2021-04-16 16:22:45 +0300

    xlsxToJson component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,19 @@
 import { Route, Switch, useHistory } from 'react-router';
 import MyHeader from './UI/MyHeader/MyHeader';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import { Layout, Menu, DatePicker } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined, FileExcelOutlined, VideoCameraOutlined, FileTextOutlined, SettingOutlined } from '@ant-design/icons';
+import { Layout } from 'antd';
 import './App.css';
-import { Home } from './UI/Home/Home';
 import { TermsOfUse } from "./UI/TermsOfUse/TermsOfUse";
-import { ReselectTesting } from './UI/ReselectTesting/ReselectTesting';
-import { ThirdAndFourthDegrees } from './UI/ThirdAndFourthDegrees/ThirdAndFourthDegrees';
-import { EkasuiReport } from './UI/EkasuiReport/EkasuiReport';
-import { ConvertVideo } from './UI/ConvertVideo/ConvertVideo';
-import { Telegrams } from './UI/Telegrams/Telegrams';
 import { WithTermsOfUse } from "./HOC/WithTermsOfUse";
 import { MySideBar } from "./UI/MySideBar/MySideBar"
 import { GeneralSettingsMainComponent } from './UI/Work/Settings/GeneralSettings/GeneralSettingsMainComponent';
 import { FilesUploadMainComponent } from './UI/Work/FilesUpload/FilesUploadMainComponent';
-import { useEffect } from 'react';
+import { ThirdAndFourthDegrees } from "./UI/Work/Reports/ThirdAndFourthDegrees/ThirdAndFourthDegrees"
+import { XlsxToJson } from './UI/Work/Tests/XlsxToJson/XlsxToJson';
 
 const { Content } = Layout;
 
 const App = () => {
-  
-  // let history = useHistory();
-  // let currentLocation = "";
-  // currentLocation = history.location.pathname;
-  // currentLocation = currentLocation.split("/")[1];
-  // debugger
-
-  // useEffect(() => {
-  //   currentLocation = history.location.pathname;
-  //   currentLocation = currentLocation.split("/")[1];
-  //   const bool = currentLocation === "work";
-  //   debugger
-  // }, [history]);
-  
-
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
@@ -43,8 +22,6 @@ const App = () => {
 
       <Content style={{ padding: '0 50px' }}>
         <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
-
-          {/* {currentLocation === "work" ? <MySideBar /> : null} */}
 
           <MySideBar />
 
@@ -55,6 +32,8 @@ const App = () => {
                 <Route exact path='/' render={() => <TermsOfUse />} />
                 <Route exact path='/work/load-books' render={() => <WithTermsOfUse Component={FilesUploadMainComponent} />} />
                 <Route exact path='/work/general-settings' render={() => <WithTermsOfUse Component={GeneralSettingsMainComponent} />} />
+                <Route exact path='/work/reports/third-and-fourth-defrees' render={() => <WithTermsOfUse Component={ThirdAndFourthDegrees} />} />
+                <Route exact path='/work/excel-to-json' render={() => <WithTermsOfUse Component={XlsxToJson} />} />
 
               </div>
             </Switch>
---
+++ src/UI/Work/Reports/ThirdAndFourthDegrees/ThirdAndFourthDegrees.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { createAndUploadWorkBook } from "../../../../helpers/common/createAndUploadWorkBook/createAndUploadWorkBook";
+import { selectIsWorkBookDataLoaded, selectReportForDay, selectCalculatedDataThirdAndFourthDegrees } from "../../../../state/features/workBookData/selectors";
+import { Alert } from 'antd';
+
+export const ThirdAndFourthDegrees = () => {
+
+    // -------------------------------------------------------------- Хуки ---------------------------------------------------------------------------
+    const isDataLoaded = useSelector(selectIsWorkBookDataLoaded);                                       // загружны ли данные в стейт
+    const calculatingData = useSelector(selectCalculatedDataThirdAndFourthDegrees);                     // вычисленные данные для отчета на этой странице
+    const reportForDate = useSelector(selectReportForDay);
+    // -------------------------------------------------------------- / Хуки -------------------------------------------------------------------------
+
+
+    // ------------------------------------ Declare функцию вызывающуюся при нажатии на кнопку для выгрузки третьих степеней ------------------------------------------------
+    const onThirdDegreesSaveButtonClick = () => {
+
+        const data = calculatingData.thirdAndFourthDegreesAoA;            // данные из селектора - массив массивов для формирования отчетной xlsx книги
+
+        createAndUploadWorkBook(                                            // Создает и предлагает скачать юзеру книгу со сформированным отчетом
+            data,                                                           // данные для записи
+            "1. 3 и 4 степени.xlsx",                                        // имя создаваемой отчетной книги
+            "3 и 4 степени"                                                 // имя листа в этой книге
+        );
+    }
+    // ------------------------------------ Declare функцию вызывающуюся при нажатии на кнопку для выгрузки третьих степеней ------------------------------------------------
+
+    return (
+        <>
+            {
+                reportForDate === ""
+                ? <Alert message="Не выбрана дата для отчета. Выберите дату во вкладке &ldquo;Настройки и установки&rdquo; &rarr; &ldquo;Основные настройки&rdquo;" type="success" type="error" showIcon />
+                : null
+            }
+            {
+                isDataLoaded && reportForDate !== ""
+                    ? <button onClick={onThirdDegreesSaveButtonClick}>Загрузить файл с 3 и 4 степенями</button>
+                    : <Alert message="Файл с данными за текущий период не загружен, сначала загрузите файл. Загрузите файл с данными за текущий период во вкладке &ldquo;Загрузка Файлов&rdquo; &rarr; &ldquo;ГРК&rdquo;" type="success" type="error" showIcon />
+            }
+        </>
+
+
+    );
+}
